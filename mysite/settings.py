50861f9a5c06c7ea4372ba155a99a0462556cc1c
HenryChinask1 <jeffbaier72@yahoo.com>
2024-07-16 15:25:30 -0400

    ok

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e+s7w28^*qrrv7v)c^fsr!t-a85(t^#t*^f04k=o@w$hkuxp7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["HenryChinask1.pythonanywhere.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
